00488d8087dde341b7d25b0549c6857c1e2808c5
Zach Hipsher <HipsherZ@bv.com>
2019-07-18 16:18:18 -0500

    Added Ranked grading logic

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,27 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+            var twentyPercent = (int)Math.Ceiling(Students.Count * 0.2);
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[twentyPercent - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(twentyPercent * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(twentyPercent * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(twentyPercent * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
